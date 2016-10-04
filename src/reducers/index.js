13dcc063dfb14a1d18f4956ab7cfa64f0150045f
Erin Knight <erin.knight@headspring.com>
2016-10-04 14:51:45 -0500

    Show preloader on ajax calls

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
 import authors from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
   courses, /* name of this property is how you will access the reducer in the app */
-  authors
+  authors,
+  ajaxCallsInProgress
 });
 
 export default rootReducer;
