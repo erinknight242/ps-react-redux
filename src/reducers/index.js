ef5a0762cc7e3958a14c19d8bf59b2392841ca3e
Erin Knight <erin.knight@headspring.com>
2016-10-04 13:49:59 -0500

    add load authors action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
+import authors from './authorReducer';
 
 const rootReducer = combineReducers({
-  courses /* name of this property is how you will access the reducer in the app */
+  courses, /* name of this property is how you will access the reducer in the app */
+  authors
 });
 
 export default rootReducer;
